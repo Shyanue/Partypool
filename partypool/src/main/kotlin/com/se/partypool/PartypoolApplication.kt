49db35497e973ce1a0aa20b5bf3e8220e24b2660
Shyanue <Shyanue@gmail.com>
2021-11-04 18:34:17 +0900

    init

---
+++ partypool/src/main/kotlin/com/se/partypool/PartypoolApplication.kt
@@ -0,0 +1,11 @@
+package com.se.partypool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PartypoolApplication
+
+fun main(args: Array<String>) {
+	runApplication<PartypoolApplication>(*args)
+}
---
+++ partypool/src/test/kotlin/com/se/partypool/PartypoolApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.se.partypool
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PartypoolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
