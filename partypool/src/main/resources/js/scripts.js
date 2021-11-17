149ebe2992a68de30e29677f87cf05b145d2b9ea
Cat-Hanbit <hello.git.GitHunb@gmail.com>
2021-11-17 17:53:47 +0900

    js 수정

--- partypool/src/main/resources/js/scripts.js
+++ partypool/src/main/resources/js/scripts.js
@@ -69,7 +69,8 @@ reader.onload = (readerEvent) => {
     document.querySelector("#img_section").setAttribute("src", readerEvent.target.result);
     //파일을 읽는 이벤트가 발생하면 img_section의 src 속성을 readerEvent의 결과물로 대체함
 };
-if(document.getElementById("#upload_file")){
+
+
 document.querySelector("#upload_file").addEventListener("change", (changeEvent) => {
     //upload_file 에 이벤트리스너를 장착
 
@@ -77,6 +78,5 @@ document.querySelector("#upload_file").addEventListener("change", (changeEvent) 
     reader.readAsDataURL(imgFile);
     //업로드한 이미지의 URL을 reader에 등록
 })
-}
- 
+
   
